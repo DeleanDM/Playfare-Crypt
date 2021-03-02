64a84b306296954e549b1fb690ce5aef06b60eae
dellsollmarr <dellsollmarr@gmail.com>
2021-03-02 21:39:28 +0200

    Добавьте файлы проекта.

---
+++ Playfare crypt/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Playfare_crypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textArea = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.matrixPanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resArea = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.GoButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.keyArea = new System.Windows.Forms.TextBox();
+            this.actionLabel = new System.Windows.Forms.Label();
+            this.changeKeyTip = new System.Windows.Forms.ToolTip(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.bigramsTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textArea
+            // 
+            this.textArea.Location = new System.Drawing.Point(12, 62);
+            this.textArea.Name = "textArea";
+            this.textArea.Size = new System.Drawing.Size(577, 154);
+            this.textArea.TabIndex = 0;
+            this.textArea.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "text";
+            // 
+            // matrixPanel
+            // 
+            this.matrixPanel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.matrixPanel.Location = new System.Drawing.Point(10, 240);
+            this.matrixPanel.Name = "matrixPanel";
+            this.matrixPanel.Size = new System.Drawing.Size(216, 199);
+            this.matrixPanel.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Матрица шифрования";
+            // 
+            // resArea
+            // 
+            this.resArea.Location = new System.Drawing.Point(353, 417);
+            this.resArea.Name = "resArea";
+            this.resArea.ReadOnly = true;
+            this.resArea.Size = new System.Drawing.Size(236, 22);
+            this.resArea.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label3.Location = new System.Drawing.Point(247, 421);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "результат:";
+            // 
+            // GoButton
+            // 
+            this.GoButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.GoButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.GoButton.Location = new System.Drawing.Point(353, 324);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(236, 50);
+            this.GoButton.TabIndex = 6;
+            this.GoButton.Text = "Рассчитать";
+            this.GoButton.UseVisualStyleBackColor = false;
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label4.Location = new System.Drawing.Point(247, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "действие:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label5.Location = new System.Drawing.Point(247, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ключ:";
+            // 
+            // keyArea
+            // 
+            this.keyArea.Location = new System.Drawing.Point(353, 224);
+            this.keyArea.Name = "keyArea";
+            this.keyArea.Size = new System.Drawing.Size(236, 22);
+            this.keyArea.TabIndex = 9;
+            this.keyArea.TextChanged += new System.EventHandler(this.keyArea_TextChanged);
+            // 
+            // actionLabel
+            // 
+            this.actionLabel.AutoSize = true;
+            this.actionLabel.Location = new System.Drawing.Point(350, 262);
+            this.actionLabel.Name = "actionLabel";
+            this.actionLabel.Size = new System.Drawing.Size(46, 17);
+            this.actionLabel.TabIndex = 11;
+            this.actionLabel.Text = "action";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label6.Location = new System.Drawing.Point(248, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 18);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "биграмы:";
+            // 
+            // bigramsTextBox
+            // 
+            this.bigramsTextBox.Location = new System.Drawing.Point(353, 390);
+            this.bigramsTextBox.Name = "bigramsTextBox";
+            this.bigramsTextBox.ReadOnly = true;
+            this.bigramsTextBox.Size = new System.Drawing.Size(236, 22);
+            this.bigramsTextBox.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 451);
+            this.Controls.Add(this.bigramsTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.actionLabel);
+            this.Controls.Add(this.keyArea);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.GoButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resArea);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.matrixPanel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textArea);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Шифр плейфера";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textArea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel matrixPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox resArea;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox keyArea;
+        private System.Windows.Forms.Label actionLabel;
+        private System.Windows.Forms.ToolTip changeKeyTip;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox bigramsTextBox;
+    }
+}
+
---
+++ Playfare crypt/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Playfare_crypt
+{
+    public partial class Form1 : Form
+    {
+        Label[,] matrix = new Label[5, 5];
+        bool actionEncrypt = true; // true = encrypt | false = decrypt 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            actionEncrypt = true;
+            actionLabel.Text = "Зашифровать";
+            label1.Text = "Текст";
+            label1.Location = new Point(200, 35);
+
+            actionLabel.Text = "Зашифровать";
+            createMatrix();
+            fillMatrix(keyArea.Text);
+        }
+        void fillMatrix(string keyString)
+        {
+            char[] alphabet = new char[25] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' }; // i = j
+            keyString = Regex.Replace(keyString, @"\s", ""); // remove all whitespaces from string via regex
+            keyString = keyString.Replace('j', 'i');
+
+            string suitableKey = string.Empty;
+            for (int i = 0; i < keyString.Length; i++)
+            {
+                if (!suitableKey.Contains(keyString[i]))
+                {
+                    suitableKey += keyString[i];
+                }
+            }
+            // MessageBox.Show(suitableKey);
+            char[] key = suitableKey.ToCharArray();
+
+            int alphabetCounter = 0;
+            int keyCharCounter = 0;
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (keyCharCounter != key.Length)
+                        {
+                            matrix[i, j].Text = key[keyCharCounter].ToString();
+                            int letterIndex = Array.IndexOf(alphabet, Convert.ToChar(matrix[i, j].Text)); // find in the alphabet the letter we`ve just add 
+                            alphabet[letterIndex] = '-';
+                            keyCharCounter++;
+                        }
+                        else
+                        {
+                            while (alphabet[alphabetCounter] == '-')
+                            {
+                                alphabetCounter++;
+                            }
+                            matrix[i, j].Text = alphabet[alphabetCounter].ToString();
+                            alphabetCounter++;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                keyArea.Text = string.Empty;
+                MessageBox.Show("Только маленькие латинские буквы");
+            }
+        }
+        void createMatrix()
+        {
+            int xLocation = 0; // start X location
+            int yLocation = 0; // start Y location
+            int xAmount = 5; // кол-во клеточек по Х
+            int yAmount = 5; // кол-во клеточек по У
+            for (int i = 0; i < yAmount; i++)
+            {
+                xLocation = 0; // возвращаем значение по умолчанию для новой строки
+                for (int j = 0; j < xAmount; j++)
+                {
+                    matrix[i, j] = new Label();
+                    matrix[i, j].Text = "X";
+                    matrix[i, j].Parent = matrixPanel;
+                    matrix[i, j].Location = new Point(xLocation, yLocation);
+                    matrix[i, j].Size = new Size(25, 25);
+                    xLocation += 35;
+                }
+                yLocation += 35;
+            }
+        }
+
+        private void keyArea_TextChanged(object sender, EventArgs e)
+        {
+            fillMatrix(keyArea.Text);
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            List<string> bigrams = createBigrams(textArea.Text);
+            string result = string.Empty;
+            if (actionEncrypt)
+            {
+                try
+                {
+                    result = encrypt(bigrams);
+                }
+                catch
+                {
+                    textArea.Text = string.Empty;
+                    MessageBox.Show("Только маленькие латинские буквы");
+                }
+            }
+            else
+            {
+                try
+                {
+                    result = decrypt(bigrams);
+                }
+                catch 
+                {
+
+                    throw;
+                }
+            }
+            resArea.Text = result;
+        }
+        string decrypt(List<string> bigrams)
+        {
+            string result = string.Empty;
+            return result;
+        }
+        string encrypt(List<string> bigrams)
+        {
+            List<string> encyptedBigram = new List<string>();
+            string result = string.Empty;
+
+            int xCordF = -1;
+            int yCordF = -1;
+            int xCordS = -1;
+            int yCordS = -1;
+
+            for (int i = 0; i < bigrams.Count; i++)
+            {
+                char[] singleBigram = bigrams[i].ToCharArray();
+                char[] singleEncryptedBigram = new char[2];
+                getLetterCords(singleBigram, out xCordF, out yCordF, out xCordS, out yCordS);
+
+                if (yCordF == yCordS) // если буквы находятся в одной колоне, то сдвигаем их на 1 вниз
+                {
+                    if (xCordF != 4 && xCordS != 4)
+                    {
+                        singleEncryptedBigram[0] = Convert.ToChar(matrix[xCordF + 1, yCordF].Text);
+                        singleEncryptedBigram[1] = Convert.ToChar(matrix[xCordS + 1, yCordS].Text);
+                        encyptedBigram.Add(string.Concat(singleEncryptedBigram[0], singleEncryptedBigram[1]));
+                        //MessageBox.Show(string.Join(" ", encyptedBigram[i]));
+                        result = string.Join("", encyptedBigram);
+                    }
+                    else if (xCordF == 4)
+                    {
+                        singleEncryptedBigram[0] = Convert.ToChar(matrix[0, yCordF].Text);
+                        singleEncryptedBigram[1] = Convert.ToChar(matrix[xCordS + 1, yCordS].Text);
+                        encyptedBigram.Add(string.Concat(singleEncryptedBigram[0], singleEncryptedBigram[1]));
+                        //MessageBox.Show(string.Join(" ", encyptedBigram[i]));
+                        result = string.Join("", encyptedBigram);
+                    }
+                    else if (xCordS == 4)
+                    {
+                        singleEncryptedBigram[0] = Convert.ToChar(matrix[xCordF + 1, yCordF].Text);
+                        singleEncryptedBigram[1] = Convert.ToChar(matrix[0, yCordS].Text);
+                        encyptedBigram.Add(string.Concat(singleEncryptedBigram[0], singleEncryptedBigram[1]));
+                        //MessageBox.Show(string.Join(" ", encyptedBigram[i]));
+                        result = string.Join("", encyptedBigram);
+                    }
+                }
+                else if (xCordF == xCordS) // если буквы находятся в одном ряду , то сдвигаем их на 1 вправо
+                {
+                    if (yCordF != 4 && yCordS != 4)
+                    {
+                        singleEncryptedBigram[0] = Convert.ToChar(matrix[xCordF, yCordF + 1].Text);
+                        singleEncryptedBigram[1] = Convert.ToChar(matrix[xCordS, yCordS + 1].Text);
+                        encyptedBigram.Add(string.Concat(singleEncryptedBigram[0], singleEncryptedBigram[1]));
+                        //MessageBox.Show(string.Join(" ", encyptedBigram[i]));
+                        result = string.Join("", encyptedBigram);
+                    }
+                    else if (yCordF == 4)
+                    {
+                        singleEncryptedBigram[0] = Convert.ToChar(matrix[xCordF, 0].Text);
+                        singleEncryptedBigram[1] = Convert.ToChar(matrix[xCordS, yCordS + 1].Text);
+                        encyptedBigram.Add(string.Concat(singleEncryptedBigram[0], singleEncryptedBigram[1]));
+                        //MessageBox.Show(string.Join(" ", encyptedBigram[i]));
+                        result = string.Join("", encyptedBigram);
+                    }
+                    else if (yCordS == 4)
+                    {
+                        singleEncryptedBigram[0] = Convert.ToChar(matrix[xCordF, yCordF + 1].Text);
+                        singleEncryptedBigram[1] = Convert.ToChar(matrix[xCordS, 0].Text);
+                        encyptedBigram.Add(string.Concat(singleEncryptedBigram[0], singleEncryptedBigram[1]));
+                        //MessageBox.Show(string.Join(" ", encyptedBigram[i]));
+                        result = string.Join("", encyptedBigram);
+                    }
+                }
+                else
+                {
+                    singleEncryptedBigram[0] = Convert.ToChar(matrix[xCordF,yCordS].Text);
+                    singleEncryptedBigram[1] = Convert.ToChar(matrix[xCordS,yCordF].Text);
+                    encyptedBigram.Add(string.Concat(singleEncryptedBigram[0], singleEncryptedBigram[1]));
+                    //MessageBox.Show(string.Join(" ", encyptedBigram[i]));
+                    result = string.Join("", encyptedBigram);
+                }
+            }
+            return result;
+        }
+        void getLetterCords(char[] singleBigram, out int xPositionFirstSign, out int yPositionFirstSign, out int xPositionSecondSign, out int yPositionSecondSign)
+        {
+            xPositionFirstSign = -1;
+            yPositionFirstSign = -1;
+
+            xPositionSecondSign = -1;
+            yPositionSecondSign = -1;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (singleBigram[0].ToString() == matrix[i, j].Text)
+                    {
+                        xPositionFirstSign = i;
+                        yPositionFirstSign = j;
+                    }
+                    if (singleBigram[1].ToString() == matrix[i, j].Text)
+                    {
+                        xPositionSecondSign = i;
+                        yPositionSecondSign = j;
+                    }
+                }
+            }
+            //MessageBox.Show(
+            //    "first letter is: " + matrix[xPositionFirstSign, yPositionFirstSign].Text + "\n" +
+            //    "second letter is: " + matrix[xPositionSecondSign, yPositionSecondSign].Text + "\n" +
+            //    "[" + xPositionFirstSign + " " + yPositionFirstSign + "]" + "  " + "[" + xPositionSecondSign + " " + yPositionSecondSign + "]"
+            //);
+        }
+        List<string> createBigrams(string text)
+        {
+            List<string> bigrams = new List<string>();
+
+            string reformedText = text;
+            reformedText = Regex.Replace(reformedText, @"\s", "");
+            for (int i = 1; i < reformedText.Length; i++)
+            {
+                int j = i - 1;
+                if (reformedText[j] == reformedText[i])
+                {
+                    reformedText = reformedText.Insert(i, "x");
+                    i++;
+                }
+            }
+
+            if (reformedText.Length % 2 != 0)
+            {
+                reformedText = reformedText.Insert(reformedText.Length, "x");
+            }
+
+            for (int i = 0; i < reformedText.Length; i += 2)
+            {
+                string temp = string.Concat(reformedText[i], reformedText[i + 1]);
+                bigrams.Add(temp);
+            }
+            bigramsTextBox.Text = string.Join(" ", bigrams);
+            return bigrams;
+        }
+
+        private void зашифроватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            actionEncrypt = true;
+            actionLabel.Text = "Зашифровать";
+            label1.Text = "Текст";
+            label1.Location = new Point(200,35);
+        }
+
+        private void расшифроватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            actionEncrypt = false;
+            actionLabel.Text = "Расшифровать";
+            label1.Text = "Зашифрованый текст";
+            label1.Location = new Point(170, 35);
+        }
+    }
+}
